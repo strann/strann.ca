4d56ebff2e49c1b3689f96f23893e9a34701d22c
Stuart Trann <strann@gmail.com>
2014-01-04 16:59:53 -0800

    Footer links, removed unnecessary grunt copy targets

--- grunt/config/copy.js
+++ grunt/config/copy.js
@@ -1,20 +1,4 @@
 module.exports = {
-  fonts: {
-    files: [{
-      expand: true,
-      cwd: '<%= dir.fontsSrc %>',
-      src: '**',
-      dest: '<%= dir.fontsDist %>'
-    }]
-  },
-  images: {
-    files: [{
-      expand: true,
-      cwd: '<%= dir.imgSrc %>',
-      src: '**',
-      dest: '<%= dir.imgDist %>'
-    }]
-  },
   js: {
     files: [{
       expand: true,
